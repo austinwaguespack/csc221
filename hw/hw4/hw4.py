9e8d7bdc52f042b55ee19f00201a19cb7d70db65
Austin Waguespack <auswag56@gmail.com>
2016-03-30 15:32:52 +0000

    adding hw4

--- hw/hw4/hw4.py
+++ hw/hw4/hw4.py
@@ -1,85 +1,79 @@
 #!/usr/bin/env python3
 
 import hw4_solution
+
 # --------------------------------------------------------------------
 # Problem 1
-# 
+#
 # Create a function, is_odd, that takes a number and returns True if
 # that number is odd.
-# 
+#
 # Function: is_odd
-# 
+#
 # parameters:
 # - number
 #
 # returns: boolean
-def is_odd(number):
-    if number%2==1:
-        return True
-    else:
-        return False
+def test_is_odd():
+    assert hw4_solution.is_odd(4) == False
+    assert hw4_solution.is_odd(3) == True
 
 # --------------------------------------------------------------------
 # Problem 2
-# 
+#
 # Create a function, is_even, that takes a number and returns True if
-# that number is even. 
-# 
+# that number is even.
+#
 # Function: is_even
-# 
+#
 # parameters:
 # - number
 #
 # returns: boolean
-def is_even(number):
-    if number%2==0:
-        return True
-    else:
-        return False
-
+def test_is_even():
+    assert hw4_solution.is_even(4) == True
+    assert hw4_solution.is_even(3) == False
 
 # --------------------------------------------------------------------
 # Problem 3
-# 
+#
 # Create a function, is_mult_of_four, that takes a number and returns
-# True if that number is a multiple of four. 
-# 
+# True if that number is a multiple of four.
+#
 # Function: is_mult_of_four
-# 
+#
 # parameters:
 # - number
 #
 # returns: boolean
-def is_mult_of_four(number):
-    if number/4==0:
-        return True
-    else:
-        return False
+def test_is_mult_of_four():
+    assert hw4_solution.is_mult_of_four(4) == True
+    assert hw4_solution.is_mult_of_four(5) == False
+
 
 
 # --------------------------------------------------------------------
 # Problem 4
-# 
+#
 # Create a function, is_mult_of_x, that takes a number and a divisor
 # and returns True if that number is divisible by divisor
-# 
+#
 # Function: is_mult_of_divisor
-# 
+#
 # parameters:
 # - number
 # - divisor
 #
 # returns: boolean
-def is_mult_of_divisor(number, divisor):
-    if number/divisor==0:
-        return True
-    else:
-        return False
+def test_is_mult_of_divisor():
+    assert hw4_solution.is_mult_of_divisor(4, 2) == True
+    assert hw4_solution.is_mult_of_divisor(5, 2) == False
+
 
 
 # --------------------------------------------------------------------
 # Problem 5
-# 
+#
 # Given a string s, return a string made of the first 2 and the last 2
 # chars of the original string, so 'spring' yields 'spng'. However, if
 # the string length is less than 2, return instead the empty string.
@@ -90,8 +84,10 @@ def is_mult_of_divisor(number, divisor):
 # - s
 #
 # returns: string
-def both_ends(s):
-    if string >2:
-        return ''
-    else:
-        return s[1, 2, -1, -2]
+def test_both_ends():
+    assert hw4_solution.both_ends('spring') == ('spng')
+    assert hw4_solution.both_ends('abd') == ('abbc')
+    assert hw4_solution.both_ends('ab') == ('abab')
+    assert hw4_solution.both_ends('abcd') == ('abcd')
+    assert hw4_solution.both_ends('') == ('')
+
